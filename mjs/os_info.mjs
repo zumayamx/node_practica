0f56fbe9a47e565973064736c9bef7a303449168
zumayamx <josezumayamx@MacBook-Air-de-Jose-3.local>
2023-12-05 21:33:19 -0600

    sample app

---
+++ mjs/os_info.mjs
@@ -0,0 +1,17 @@
+
+ /* import os from 'node:os'  <--- mas generico recuerda agregar objeto en cada llamada de función, ex os.platform */
+
+/* tambien puedes utilizar */
+import { platform, release, totalmem, freemem, arch, cpus, uptime } from 'node:os' 
+
+console.log('Información del sistema opetativo: ')
+console.log('---------------------------------')
+
+console.log('Nombre del sistema operativo: ', platform())
+console.log('Versión del sistema operativo: ', release())
+console.log('Memoria total: ', totalmem(), 'bytes')
+console.log('Memoria libre: ', freemem(), 'bytes')
+console.log('Arquitectura del sistema: ', arch())
+
+console.log('CPUs: ', cpus()) /* Información de los procesadores para escalar programas en node */
+console.log('Uptime: ', uptime() / 60 / 60) /* Tiempo que lleva encendido el sistema operativo */
