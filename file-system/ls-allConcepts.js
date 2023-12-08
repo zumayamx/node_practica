ca63f511e18a5ee99cc621043262fb0d15ed64b9
zumayamx <josezumayamx@MacBook-Air-de-Jose-3.local>
2023-12-07 19:46:18 -0600

    ejercicio de todos los conceptos vistos

--- file-system/ls-allConcepts.js
+++ file-system/ls-allConcepts.js
@@ -1,2 +1,8 @@
 const fs = require ('node:fs/promises')
 const path = require('node:path')   
+
+const folder = process.argv[2] || '.' /* <-- Si no se pasa argumento, se toma el directorio actual */   
+
+async function ls(folder) {
+    let files
+}
